5406dbbd4e00098095891055135c164845eb3435
willvianaa <will7viana@gmail.com>
2024-05-24 10:48:32 -0300

    First Commit

--- backend/projeto/src/main/java/br/com/crud/projeto/repositories/UsuarioRepository.java
+++ backend/projeto/src/main/java/br/com/crud/projeto/repositories/UsuarioRepository.java
@@ -2,7 +2,7 @@ package br.com.crud.projeto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository  extends JpaRepository<T>{
+import br.com.crud.projeto.Model.UsuarioModel;
 
-    
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long>{
 } 
