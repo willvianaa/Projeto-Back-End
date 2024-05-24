50091647913d1604f091d278ec7341c2e4dea69e
willvianaa <will7viana@gmail.com>
2024-05-24 10:44:23 -0300

    Nova parte

---
+++ backend/projeto/src/main/java/br/com/crud/projeto/repositories/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.com.crud.projeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository  extends JpaRepository<T>{
+
+    
+} 
